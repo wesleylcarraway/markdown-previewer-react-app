c97f929932af91b606eed35237d9c3225317c9a9
wesleylcarraway <wesleylc.office@gmail.com>
2022-01-11 01:02:31 -0300

    files organized in a better way

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Editor.css';
+import './index.css';
 
 export default (props) => {
   return (
--- src/components/Previewer/index.js
+++ src/components/Previewer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Previewer.css';
+import './index.css';
 
 export default (props) => {
   const marked = require('marked');
